621f54ff804bf10b60918bfc0477c2b7e7f49f03
jungjihyoun <jihyoun0602@gmail.com>
2022-04-11 22:43:18 +0900

    feat / splashScreen 추가

---
+++ App/screens/auth/SplashScreen.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from 'react';
+import {Button, StyleSheet, Text, StatusBar} from 'react-native';
+import {
+  Container,
+  SplashImage,
+  H1,
+  H2,
+  TextWrapper,
+  TextArea,
+  ImageArea,
+} from './auth.styled';
+
+const SplashScreen = ({navigation}) => {
+  return (
+    <Container>
+      <TextArea>
+        <TextWrapper width="227px">
+          <H1>오늘 같은 날씨에</H1>
+        </TextWrapper>
+        <TextWrapper>
+          <H1>다른 사람들은</H1>
+        </TextWrapper>
+        <TextWrapper>
+          <H1>뭐 입고 있지?</H1>
+        </TextWrapper>
+        <H2 left="20px">Weather people!</H2>
+      </TextArea>
+      <ImageArea>
+        <SplashImage source={require('../../assets/splash.png')} />
+      </ImageArea>
+    </Container>
+  );
+};
+
+export default SplashScreen;
---
+++ App/screens/auth/auth.styled.js
@@ -0,0 +1,60 @@
+import styled, {css} from 'styled-components/native';
+
+import {Text, SafeAreaView, Image, View} from 'react-native';
+
+export const Container = styled(SafeAreaView)`
+  justify-content: center;
+  align-items: center;
+  background-color: #262626;
+  height: 100%;
+  width: 100%;
+`;
+
+export const TextArea = styled(View)`
+  width: 100% 
+  flex: 1;
+  justify-content:  flex-end;
+  margin-left : 24px
+`;
+
+export const ImageArea = styled(View)`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+  position: relative;
+`;
+
+export const TextWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: ${props => props.width || '194px'}
+  height: 44px;
+  background: #84e6b1;
+  border-radius: 50px;
+  margin-bottom: 14px;
+`;
+
+export const H1 = styled(Text)`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 36px;
+  letter-spacing: -1.07135px;
+`;
+
+export const H2 = styled(Text)`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  letter-spacing: -1.07135px;
+  color: #ffffff;
+  margin-left: ${props => props && props.left};
+`;
+
+export const SplashImage = styled(Image)`
+  position : absolute
+  bottom : -7%
+  right: 0
+`;

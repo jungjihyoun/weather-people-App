0e4fcfa54b6217d66932fb6553f96fe55df1632a
jungjihyoun <jihyoun0602@gmail.com>
2022-04-12 22:01:20 +0900

    feat: 절대경로 지정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-react'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./App'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.png',
+        ],
+        alias: {
+          '@': './App',
+          '@components': './App/components',
+          '@apollo': './App/apollo',
+          '@constant': './App/constant',
+          '@graphql': './App/graphql',
+          '@store': './App/store',
+          '@containers': './App/containers',
+          '@screens': './App/screens',
+          '@utils': './App/utils',
+          '@styles': './App/styles',
+        },
+      },
+    ],
+  ],
 };

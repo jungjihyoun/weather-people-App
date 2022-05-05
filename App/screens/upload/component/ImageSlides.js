1a94dea00712325b8f0d7dea201f2adac7ce5359
jungjihyoun <jihyoun0602@gmail.com>
2022-05-05 21:28:59 +0900

    feat: 게시글 이미지 swiper 컴포넌트 추가

--- App/screens/upload/component/ImageSlides.js
+++ App/screens/upload/component/ImageSlides.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import {Text , StyleSheet , View} from 'react-native'
+import {StyleSheet , View , Image} from 'react-native'
 import Swiper from 'react-native-swiper';
-import {images , fonts} from '../../../styles/globalStyles';
-import {IconImage, TopLayout, ImageWrapper, IconText} from '../Upload.Styled';
 
-const ImageSlides = () => {
+const ImageSlides = ({photos , onOpenGallery}) => {
   return (
-    <View style={styles.slide}>
+    <View onPress={() => onOpenGallery()} style={styles.slide} >
       <Swiper showsButtons={false} showsPagination={true}>
-        <View style={styles.slide}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
-        <View style={styles.slide}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
+          {
+            photos.length > 0 ?
+              photos.map((e , index)=>{
+                return <Image key={index} resizeMode="stretch" style={styles.ImageWrapper} source={{uri: e.uri}}/>
+              }) : <View />
+          }
       </Swiper>
     </View>
   );
@@ -24,9 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#9DD6EB',
     height: 470
   },
-
+  ImageWrapper: {
+    height: 470
+  },
   menu_title: {
-    fontFamily: fonts.suitB,
+    // fontFamily: fonts.suitB,
     color: '#111111',
     textAlign: "center",
     fontSize: 18,
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     lineHeight:22,
   },
   menu_successBtn: {
-    fontFamily: fonts.suitB,
+    // fontFamily: fonts.suitB,
     color: '#A5A5A5',
     textAlign: "center",
     fontSize: 18,
